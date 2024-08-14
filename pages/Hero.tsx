3cbfeafc2161b8259c0c9739992e02a609c2e002
charans29 <smodugula@csuchico.edu>
2024-08-13 23:55:08 -0400

    dev: home page bg

---
+++ pages/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div className='h-screen'
+      style={{
+        backgroundImage: `linear-gradient(145deg, rgba(1,0,3,1) 25%, rgba(10,8,50,0.75) 130%)`,
+      }}
+    />
+  );
+}
+
+export default Hero;
