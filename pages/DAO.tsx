364bc3274d095225bc6c01f207e00ebdee52b3f7
charans29 <smodugula@csuchico.edu>
2024-08-15 22:00:37 -0400

    dev: resolution testing

--- pages/DAO.tsx
+++ pages/DAO.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function DAO() {
   return (
     <div
-    className="h-full w-10/12 -translate-y-16 rounded-xl text-center flex flex-col justify-center border-[0.25px]
+    className="h-full w-10/12 rounded-xl text-center flex flex-col justify-center border-[0.25px]
      border-red-950 text-white/60"
     style={{
       backgroundImage: `linear-gradient(300deg, rgba(234,88,88,0.1) 30%, rgba(130,110,118,0.35) 120%)`
--- pages/Hero.tsx
+++ pages/Hero.tsx
@@ -5,7 +5,7 @@ import Body from '../elements/Body';
 
 function Hero() {
   return (
-    <div className='h-screen flex flex-col justify-between'
+    <div className='h-screen flex flex-col'
     style={{
       backgroundImage: `linear-gradient(180deg, rgba(5,0,5,0.65) 30%, rgba(15,1,4,1) 70%)`
     }}
