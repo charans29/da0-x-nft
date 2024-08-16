525ed530a77a31d30165ba9256ae36a1e0c625ed
charans29 <smodugula@csuchico.edu>
2024-08-15 22:41:51 -0400

    dev: header padding check

--- elements/Header.tsx
+++ elements/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-        <div className='flex flex-row justify-between relative px-12 m-5 overflow-hidden'>
+        <div className='flex flex-row justify-between relative p-2 m-5 overflow-hidden'>
             <div className='text-green-500 font-extrabold text-sm font-sans scale-x-150'>
                 Hi, Oxcharan
             </div>
