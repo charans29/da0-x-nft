c22838bfab15a0bb0f7bebba94406ec3efd5d7d7
charans29 <smodugula@csuchico.edu>
2024-08-15 22:23:41 -0400

    dev: resolution final check

--- pages/DAOS.tsx
+++ pages/DAOS.tsx
@@ -4,7 +4,7 @@ function DAOS() {
   return (
     <div 
       className="h-full w-10/12 rounded-xl text-center flex flex-col justify-center border-[0.25px] 
-      border-red-950 text-white/60 -mt-4"
+      border-red-950 text-white/60 -mt-10"
       style={{
         backgroundImage: `linear-gradient(300deg, rgba(234,88,88,0.1) 30%, rgba(130,110,118,0.35) 120%)`
       }}
