80fee1db0fac69ccd97e4e065e9a2a9db9410063
charans29 <smodugula@csuchico.edu>
2024-08-15 22:47:41 -0400

    dev:text and padd chek

--- elements/Header.tsx
+++ elements/Header.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
-        <div className='flex flex-row justify-between relative p-2 m-5 overflow-hidden'>
-            <div className='text-green-500 font-extrabold text-sm font-sans scale-x-150'>
+        <div className='flex flex-row justify-between relative p-3 overflow-hidden'>
+            <div className='text-green-500 font-extrabold text-xs font-sans scale-x-150'>
                 Hi, Oxcharan
             </div>
-            <div className='text-purple-500 font-extrabold text-sm font-sans scale-x-150'>
+            <div className='text-purple-500 font-extrabold text-xs font-sans scale-x-150'>
                 WALLET
             </div>
         </div>
