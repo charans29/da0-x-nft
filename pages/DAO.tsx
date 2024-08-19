74bb3ee7d95044bfe8075c1fbde94b1854688f4c
charans29 <smodugula@csuchico.edu>
2024-08-18 23:20:25 -0400

    dev: nft size chk-3

--- pages/DAO.tsx
+++ pages/DAO.tsx
@@ -4,13 +4,16 @@ import React from 'react'
 function DAO() {
   return (
     <div
-    className="h-full w-10/12 rounded-xl text-center flex flex-col justify-center border-[0.25px]
+    className="h-full w-10/12 rounded-xl text-center flex flex-col justify-between border-[0.25px]
      border-red-950 text-white/60 -mt-5"
     style={{
       backgroundImage: `linear-gradient(300deg, rgba(234,88,88,0.1) 30%, rgba(130,110,118,0.35) 120%)`
     }}
   >
-      <ParallaxView/>
+    <text className='text-md text-blue-500 font-bold font-sans pt-3'>
+      SELECT NFT FOr YOUR DAO
+    </text>
+    <ParallaxView/>
     </div>
   )
 }
