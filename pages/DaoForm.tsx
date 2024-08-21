fe7b0bded2d8196a128316568cd92bbb26358a6d
charans29 <smodugula@csuchico.edu>
2024-08-20 22:50:40 -0400

    dev: form changes check

--- pages/DaoForm.tsx
+++ pages/DaoForm.tsx
@@ -12,63 +12,67 @@ function DaoForm() {
                 }}
             />
             <div className='flex flex-col justify-end'>
-                <text className='text-sm font-light'>
-                    Asset Price
+                <text className='text-xs font-mono'>
+                    Asset Price:
                 </text>
-                <text className='text-sm font-light'>
-                    Asset Value
+                <text className='text-xs font-mono'>
+                    Volume:
                 </text>
             </div>
         </div>
         <fieldset className='flex flex-col space-y-1'>
-        <legend className='text-center text-xs mb-1'>CREATE YOUR DAO</legend>
-        <label className='items-start font-sans text-xs'>DAO Name</label>
-        <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin mb-1'
-            style={{
-                backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.5) 100%, rgba(10,1,1,0.35) 0%)`,
-                width:`300px`
-              }}
-              placeholder='Arstistic Aliance'
-              type='text'
-        ></input>
-        <label className='items-start font-sans text-xs'>Fractionalization</label>
-        <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin mb-1'
-            style={{
-                backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.4) 100%, rgba(10,1,1,0.35) 0%)`,
-                width:`300px`
-              }}
-              placeholder='Total Number of Shares'
-              type='text'
-        ></input>
-        <label className='items-start font-sans text-xs'>Your Share</label>
-        <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin'
-            style={{
-                backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.5) 100%, rgba(10,1,1,0.35) 0%)`,
-                width:`300px`
-              }}
-              placeholder='SOL willing to invest'
-              type='text'
-        ></input>
-        <label className='items-start font-sans text-xs'>Signature</label>
-        <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin'
-            style={{
-                backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.5) 100%, rgba(10,1,1,0.35) 0%)`,
-                width:`300px`
-              }}
-              placeholder='Digital Signature'
-              type='text'
-        ></input>
-        <div className='flex justify-center w-full pt-1.5'>
-            <Button className='border-[0.25px] border-orange-900 text-center px-8'
-                onClick={() => {}}
+            <legend className='text-center text-xs mb-1'>CREATE YOUR DAO</legend>
+            <label className='items-start font-sans text-xs'>DAO Name</label>
+            <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin 
+                border-[0.25px] border-gray-600'
                 style={{
-                    backgroundImage: 'linear-gradient(90deg, rgba(82,0,83,1) -90%, rgba(0,12,15,1) 90%)'
+                    backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.5) 100%, rgba(10,1,1,0.35) 0%)`,
+                    width:`300px`
                 }}
-            >
-                Create
-            </Button>
-          </div>
-    </fieldset>
+                placeholder='Arstistic Aliance'
+                type='text'
+            ></input>
+            <label className='items-start font-sans text-xs'>Fractionalization</label>
+            <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin
+                border-[0.25px] border-gray-600'
+                style={{
+                    backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.4) 100%, rgba(10,1,1,0.35) 0%)`,
+                    width:`300px`
+                }}
+                placeholder='Total Number of Shares'
+                type='text'
+            ></input>
+            <label className='items-start font-sans text-xs'>Your Share</label>
+            <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin
+                border-[0.25px] border-gray-600'
+                style={{
+                    backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.5) 100%, rgba(10,1,1,0.35) 0%)`,
+                    width:`300px`
+                }}
+                placeholder='SOL willing to invest'
+                type='text'
+            ></input>
+            <label className='items-start font-sans text-xs'>Signature</label>
+            <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin
+                border-[0.25px] border-gray-600'
+                style={{
+                    backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.5) 100%, rgba(10,1,1,0.35) 0%)`,
+                    width:`300px`
+                }}
+                placeholder='Digital Signature'
+                type='text'
+            ></input>
+            <div className='flex justify-center w-full pt-1.5'>
+                <Button className='border-[0.25px] border-orange-900 text-center px-8'
+                    onClick={() => {}}
+                    style={{
+                        backgroundImage: 'linear-gradient(90deg, rgba(82,0,83,1) -90%, rgba(0,12,15,1) 90%)'
+                    }}
+                >
+                    Create
+                </Button>
+            </div>
+        </fieldset>
     </div>
   )
 }
