152a592db066b6a0e09dfec3568228a7ad7f7e49
charans29 <smodugula@csuchico.edu>
2024-08-20 20:52:33 -0400

    dev: form look check

--- pages/DaoForm.tsx
+++ pages/DaoForm.tsx
@@ -13,36 +13,36 @@ function DaoForm() {
         <fieldset className='flex flex-col space-y-1'>
         <legend className='text-center text-xs mb-1'>CREATE YOUR DAO</legend>
         <label className='items-start font-sans text-xs'>DAO Name</label>
-        <input className='rounded bg-transparent focus:outline-none text-xs py-1 px-1.5 text-white font-thin mb-1'
+        <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin mb-1'
             style={{
-                backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.3) 100%, rgba(10,1,1,0.35) 0%)`,
+                backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.5) 100%, rgba(10,1,1,0.35) 0%)`,
                 width:`300px`
               }}
               placeholder='Arstistic Aliance'
               type='text'
         ></input>
         <label className='items-start font-sans text-xs'>Fractionalization</label>
-        <input className='rounded bg-transparent focus:outline-none text-xs py-1 px-1.5 text-white font-thin mb-1'
+        <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin mb-1'
             style={{
-                backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.3) 100%, rgba(10,1,1,0.35) 0%)`,
+                backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.5) 100%, rgba(10,1,1,0.35) 0%)`,
                 width:`300px`
               }}
               placeholder='Total Number of Shares'
               type='text'
         ></input>
         <label className='items-start font-sans text-xs'>Your Sahre</label>
-        <input className='rounded bg-transparent focus:outline-none text-xs py-1 px-1.5 text-white font-thin'
+        <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin'
             style={{
-                backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.3) 100%, rgba(10,1,1,0.35) 0%)`,
+                backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.5) 100%, rgba(10,1,1,0.35) 0%)`,
                 width:`300px`
               }}
               placeholder='SOL willing to invest'
               type='text'
         ></input>
         <label className='items-start font-sans text-xs'>Signature</label>
-        <input className='rounded bg-transparent focus:outline-none text-xs py-1 px-1.5 text-white font-thin'
+        <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin'
             style={{
-                backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.3) 100%, rgba(10,1,1,0.35) 0%)`,
+                backgroundImage: `linear-gradient(180deg, rgba(50, 50, 80, 0.5) 100%, rgba(10,1,1,0.35) 0%)`,
                 width:`300px`
               }}
               placeholder='Digital Signature'
