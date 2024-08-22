ba9ab2da52898a774b08962e68dfd7d3e6b71ba3
charans29 <smodugula@csuchico.edu>
2024-08-22 00:38:52 -0400

    dev: deployment checks

--- pages/DAOS.tsx
+++ pages/DAOS.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import DAOs from './api/DAOs'
-import { motion } from "framer-motion";
 
 function DAOS() {
   return (
     <div 
-      className="h-full w-10/12 rounded-xl flex flex-col justify-center border-[0.25px] overflow-x-auto
+      className="h-full w-10/12 rounded-xl flex flex-col justify-center border-[0.25px]
       border-red-950 text-white/60 -mt-5"
       style={{
         backgroundImage: `linear-gradient(300deg, rgba(234,88,88,0.1) 30%, rgba(130,110,118,0.35) 120%)`
       }}
     >
-      <div className='flex flex-row justify-between space-x-4 px-28 cursor-pointer'>
+      <div className='flex flex-row justify-between space-x-4 px-28 cursor-pointer overflow-x-auto'
+        style={{
+          scrollbarWidth: 'none'
+        }}
+      >
         {DAOs.map((nft, idx) => (
-            <img 
+            <img key={idx}
               src={nft.asset}
               style={{height:'500px', width:'350px', borderRadius:'10px'}}
               onClick={()=>{}}
             />
-            
           ))}
       </div>
     </div>
