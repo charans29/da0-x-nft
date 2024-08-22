665ef2011de657a57ddcbe5dd7ff9fc69654d269
charans29 <smodugula@csuchico.edu>
2024-08-22 02:10:01 -0400

    dev:daos add check

--- elements/Fields.tsx
+++ elements/Fields.tsx
@@ -1,8 +1,29 @@
 import Button from '@/components/Button'
+import { useDAO } from '@/context/DaoContext';
+import DAOs, { DAO } from '@/pages/api/DAOs';
 import React, { useState } from 'react'
 
 function Fields() {
-    const [create, setCreate] = useState(false)
+    const [create, setCreate] = useState(false);
+    const [daoName, setDaoName] = useState('');
+    const [address, setAddress] = useState('');
+    const [fractions, setFractions] = useState('');
+    const [share, setShare] = useState('');
+    const { nft } = useDAO();
+
+    const handleCreate = () => {
+        const newDAO: DAO = {
+            name: daoName,
+            aaddress: address,
+            asset: nft?.image,
+            fractions: parseInt(fractions) || 0,
+            share: parseInt(share) || 0, 
+        }
+
+        DAOs.push(newDAO) 
+        setCreate(true)
+    }
+
   return (
     <fieldset className='flex flex-col space-y-1'>
         <legend className='text-center text-xs mb-1'>CREATE YOUR DAO</legend>
@@ -15,6 +36,8 @@ function Fields() {
             }}
             placeholder='Arstistic Aliance'
             type='text'
+            value={daoName}
+            onChange={(e) => setDaoName(e.target.value)}
         ></input>
         <label className='items-start font-sans text-xs'>Address</label>
         <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin
@@ -25,6 +48,8 @@ function Fields() {
             }}
             placeholder='Your Solana Wallet&apos;s'
             type='text'
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
         ></input>
         <label className='items-start font-sans text-xs'>Fractions</label>
         <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin
@@ -35,6 +60,8 @@ function Fields() {
             }}
             placeholder='Total Number of Shares'
             type='text'
+            value={fractions}
+            onChange={(e) => setFractions(e.target.value)}
         ></input>
         <label className='items-start font-sans text-xs'>Your Share</label>
         <input className='rounded bg-transparent focus:outline-none text-xs p-1.5 text-white font-thin
@@ -45,10 +72,12 @@ function Fields() {
             }}
             placeholder='SOL you willing to invest'
             type='text'
+            value={share}
+            onChange={(e) => setShare(e.target.value)}
         ></input>
         <div className='flex justify-center w-full pt-1.5'>
             <Button className='border-[0.25px] border-orange-900 text-center px-8'
-                onClick={() => {setCreate(true)}}
+                onClick={handleCreate}
                 style={{
                     backgroundImage: 'linear-gradient(90deg, rgba(82,0,83,1) -10%, rgba(0,12,15,1) 90%)'
                 }}
--- pages/DAOS.tsx
+++ pages/DAOS.tsx
@@ -1,27 +1,35 @@
 import React from 'react'
 import DAOs from './api/DAOs'
+import { AiOutlineCaretLeft, AiOutlineCaretRight } from "react-icons/ai";
 
 function DAOS() {
   return (
     <div 
-      className="h-full w-10/12 rounded-xl flex flex-col justify-center border-[0.25px]
+      className="h-full w-10/12 rounded-xl flex flex-col justify-between text-center border-[0.25px]
       border-red-950 text-white/60 -mt-5"
       style={{
         backgroundImage: `linear-gradient(300deg, rgba(234,88,88,0.1) 30%, rgba(130,110,118,0.35) 120%)`
       }}
     >
+      <text className='text-md text-blue-600 font-semibold font-sans pt-3'>
+        JOIN A DAO NFT U LIKE
+      </text>
       <div className='flex flex-row justify-between space-x-3.5 px-24 cursor-pointer overflow-x-auto'
         style={{
           scrollbarWidth: 'none'
         }}
       >
-        {DAOs.map((nft, idx) => (
+        {DAOs.slice().reverse().map((nft, idx) => (
             <img key={idx}
               src={nft.asset}
               style={{height:'250px', width:'175px', borderRadius:'10px'}}
               onClick={()=>{}}
             />
           ))}
+      </div>
+      <div className='flex justify-center pb-3 space-x-2'>
+        <AiOutlineCaretLeft className='bg-slate-700 rounded-full -p-3 cursor-pointer' style={{color:'black'}}/>
+        <AiOutlineCaretRight className='bg-slate-700 rounded-full -p-3 cursor-pointer' style={{color:'black'}}/>
       </div>
     </div>
   )
