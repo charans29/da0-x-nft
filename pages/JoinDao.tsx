ac74c602f555706d70aa06e9d76f380abf58ebae
charans29 <smodugula@csuchico.edu>
2024-08-23 14:00:56 -0400

    dev:join-pg color check2

--- pages/JoinDao.tsx
+++ pages/JoinDao.tsx
@@ -4,9 +4,9 @@ function JoinDao({ children }: { children: ReactNode }) {
 
     return (
         <div className="h-full w-full fixed inset-0 z-[100] flex items-center justify-center backdrop-blur-2xl">
-            <div className="z-20 absolute rounded-xl p-10"
+            <div className="z-20 absolute rounded-xl p-5"
                 style={{
-                    backgroundImage: `linear-gradient(300deg, rgba(88,88,234,0.35) 100%, rgba(130,110,118,0.35) 120%)`
+                    backgroundImage: `linear-gradient(300deg, rgba(88,88,234,0.25) 100%, rgba(130,110,118,0.35) 120%)`
                 }}
             >
                 {children}
