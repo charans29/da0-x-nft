d92c1dfcb6e37d810fb8c768204e91258df6e66c
charans29 <smodugula@csuchico.edu>
2024-08-25 13:06:11 -0400

    dev:confetti dao fix1

--- pages/DaoCreated.tsx
+++ pages/DaoCreated.tsx
@@ -8,13 +8,13 @@ function DaoCreated() {
         <div className='relative inset-x-[100px] inset-y-[100px]'>
           <img 
             src={"/api/image-proxy?url=https://www.arweave.net/7dntcPrPZeee702M5FBVCAOdnKVS5pGGQJVUsHKGCWI?ext=png"}
-            className="h-32 w-32 rounded-lg cursor-pointer inset-x-[550px] inset-y-[200px] "
+            className="h-28 w-28 rounded-lg cursor-pointer inset-x-[550px] inset-y-[200px] "
           />
         </div>
-        <div className="text-white-500 text-cyan-200 font-light text-center -mt-10">
+        <div className="text-white-500 text-cyan-200 font-light text-center text-xs -mt-10">
           Woohoo..! Created
         </div>
-        <div className="text-white-500 text-cyan-200 font-light mt-20 text-center">
+        <div className="text-white-500 text-cyan-200 font-light mt-20 text-xs text-center">
             DAO
         </div>
       </Confetti>
