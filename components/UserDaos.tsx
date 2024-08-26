a3422962a39a53f831e186de2aff4dcf68a99be3
charans29 <smodugula@csuchico.edu>
2024-08-26 01:43:20 -0400

    dev:Dao tiles check1

--- components/UserDaos.tsx
+++ components/UserDaos.tsx
@@ -4,32 +4,17 @@ function UserDaos() {
   return (
     <div 
         className={`absolute top-16 right-10 h-[calc(100vh-110px)] w-[200px] 
-            p-5 z-30 rounded-lg border-green-500 border-[0.25px] overflow-hidden`}
+            p-5 z-30 rounded-lg border-gray-700 border-[0.25px] overflow-hidden`}
         style={{
-            backgroundImage:'linear-gradient(300deg, rgba(84,48,188,0.5) 10%, rgba(150,100,180,0.8) 100%)',
+            backgroundImage:'linear-gradient(180deg, rgba(50,40,80,0.75) 10%, rgba(50,100,80,0.75) 130%)',
         }}
     >
-        <h2 className='text-lg text-black font-bold mb-4 sticky top-0 p-2 text-center z-10'>
+        <h2 className='text-md text-white/50 font-light mb-4 sticky top-0 p-2 text-center z-10'>
             Your DAOS
         </h2>
-        <ul className='realtive h-full overflow-y-auto -mt-3 z-0' style={{scrollbarWidth: 'none'}}>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
-            <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
+        <ul className='realtive h-full overflow-y-auto mt-5 z-0' style={{scrollbarWidth: 'none'}}>
+            <li className='mb-3 border-[1.25px] rounded-lg p-1 px-2 border-zinc-600 text-xs text-purple-300'>DAO 1</li>
+            <li className='mb-3 border-[1.25px] rounded-lg p-1 px-2 border-zinc-600 text-xs text-purple-300'>DAO 2</li>
         </ul>
     </div>
   )
