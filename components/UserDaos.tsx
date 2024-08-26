6902dc5ea12196c02819d8297c4376e3ad00f698
charans29 <smodugula@csuchico.edu>
2024-08-26 00:39:07 -0400

    dev:Dao tiles check

--- components/UserDaos.tsx
+++ components/UserDaos.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 function UserDaos() {
   return (
     <div 
-        className={`absolute top-16 right-10 h-[calc(100vh-110px)] w-[300px] 
-            p-5 z-30 rounded-lg`}
+        className={`absolute top-16 right-10 h-[calc(100vh-110px)] w-[200px] 
+            p-5 z-30 rounded-lg border-green-500 border-[0.25px] overflow-hidden`}
         style={{
-            backgroundImage:'linear-gradient(300deg, rgba(234,88,88,0.1) 30%, rgba(130,110,118,0.35) 120%)',
+            backgroundImage:'linear-gradient(300deg, rgba(84,48,188,0.5) 10%, rgba(150,100,180,0.8) 100%)',
         }}
     >
-        <h2 className='text-lg text-black font-bold mb-4 sticky top-0 p-2 text-center'>
+        <h2 className='text-lg text-black font-bold mb-4 sticky top-0 p-2 text-center z-10'>
             Your DAOS
         </h2>
-        <ul className=' h-full overflow-y-auto -mt-3' style={{scrollbarWidth: 'none'}}>
+        <ul className='realtive h-full overflow-y-auto -mt-3 z-0' style={{scrollbarWidth: 'none'}}>
             <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
             <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
             <li className='mb-2 border-[1.25px] rounded-md p-2 border-black text-black'>Item 1</li>
