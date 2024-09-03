88825d432dd9f9461575b1fae3332089e183117e
charans29 <smodugula@csuchico.edu>
2024-09-03 14:47:53 -0400

    dev:inspect actions

---
+++ app/actions.json/routes.ts
@@ -0,0 +1,25 @@
+
+import { createActionHeaders, type ActionsJson } from "@solana/actions";
+
+export const GET = async () => {
+  const payload: ActionsJson = {
+    rules: [
+      // map all root level routes to an action
+      {
+        pathPattern: "/*",
+        apiPath: "/api/actions/*",
+      },
+      // idempotent rule as the fallback
+      {
+        pathPattern: "/api/actions/**",
+        apiPath: "/api/actions/**",
+      },
+    ],
+  };
+
+  return Response.json(payload, {
+    headers: createActionHeaders(),
+  });
+};
+
+export const OPTIONS = GET;
