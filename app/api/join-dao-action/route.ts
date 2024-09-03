d648e748e3bf6283201f63729b1bf840f9684240
charans29 <smodugula@csuchico.edu>
2024-09-03 19:17:14 -0400

    ft:blink endpoint check4

---
+++ app/api/join-dao-action/route.ts
@@ -0,0 +1,35 @@
+import DAOs from "@/pages/api/DAOs";
+import {
+    ActionGetResponse,
+    ACTIONS_CORS_HEADERS,
+  } from "@solana/actions";
+
+  
+  export async function GET(request: Request){
+    const requestURL = new URL(request.url);
+    const id = requestURL.searchParams.get('id');
+    const daoIndex = parseInt(id ?? "");
+    const dao = DAOs[daoIndex];
+    const { count, fractions, asset } = dao;
+    const iconURL = new URL(asset ?? "", requestURL.origin);
+  
+    const payload: ActionGetResponse = {
+      icon: iconURL.toString(),
+      description: `Join this DAO. Current members: ${count}/${fractions}`,
+      title: "Join DAO",
+      label: "Join DAO",
+      links: {
+        actions: [
+          {
+            label: "Join",
+            href: ``,
+          },
+        ],
+      },
+    };
+  
+    const response = Response.json(payload, {headers: ACTIONS_CORS_HEADERS})
+
+    return response
+  
+  }
