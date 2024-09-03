00608663c1a90a6209517b27f37dbb60d4bfa9eb
charans29 <smodugula@csuchico.edu>
2024-09-03 18:14:10 -0400

    ft:blink endpoint check

--- pages/api/join-dao-action.ts
+++ pages/api/join-dao-action.ts
@@ -1,39 +1,35 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
 import {
-  createActionHeaders,
-  ActionGetResponse,
-} from "@solana/actions";
-import DAOs from './DAOs';
+    ActionGetResponse,
+    ACTIONS_CORS_HEADERS,
+  } from "@solana/actions";
+  import DAOs from './DAOs';
+  
+  export const GET = (request: Request) => {
+    const requestURL = new URL(request.url);
+    const id = requestURL.searchParams.get('id');
+    const daoIndex = parseInt(id ?? "");
+    const dao = DAOs[daoIndex];
+    const { count, fractions, asset } = dao;
+    const iconURL = new URL(asset ?? "", requestURL.origin);
+  
+    const payload: ActionGetResponse = {
+      type: "action",
+      title: "Join DAO",
+      description: `Join this DAO. Current members: ${count}/${fractions}`,
+      icon: iconURL.toString(),
+      label: "Join DAO",
+      links: {
+        actions: [
+          {
+            label: "Join",
+            href: ``,
+          },
+        ],
+      },
+    };
+  
+    const response = Response.json(payload, {headers: ACTIONS_CORS_HEADERS})
 
-export const GET = (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => {
-  const headers = createActionHeaders();
-  const { id } = req.query;
-  const daoIndex = parseInt(id as string);
-  const dao = DAOs[daoIndex];
-  const { count, fractions, asset } = dao;
-
-  const payload: ActionGetResponse = {
-    type: "action",
-    title: "Join DAO",
-    description: `Join this DAO. Current members: ${count}/${fractions}`,
-    icon: asset as string,
-    label: "Join DAO",
-    links: {
-      actions: [
-        {
-          label: "Join",
-          href: `https://your-backend-url.com/process-join?asset=${asset}&count=${count}&fractions=${fractions}`,
-        },
-      ],
-    },
-  };
-
-  return res.status(200).json(payload), {
-    headers,
-  };
-}
-
-export const OPTIONS = GET;
+    return response
+  
+  }
