42754d13343f3dccee4b37294376815fe2ab1d87
charans29 <smodugula@csuchico.edu>
2024-09-03 14:54:02 -0400

    dev:inspecting actions2

--- app/actions.json/routes.ts
+++ app/actions.json/routes.ts
@@ -7,12 +7,12 @@ export const GET = async () => {
       // map all root level routes to an action
       {
         pathPattern: "/*",
-        apiPath: "/api/actions/*",
+        apiPath: "/pages/api/*",
       },
       // idempotent rule as the fallback
       {
-        pathPattern: "/api/actions/**",
-        apiPath: "/api/actions/**",
+        pathPattern: "/pages/api/**",
+        apiPath: "/pages/api/**",
       },
     ],
   };
