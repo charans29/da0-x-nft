dbe3ac214e3751578b8cd90d1b335f885b1963d0
charans29 <smodugula@csuchico.edu>
2024-09-03 20:04:22 -0400

    ft:post endpoint-val check@dialect

--- app/api/join-dao-action/route.ts
+++ app/api/join-dao-action/route.ts
@@ -22,7 +22,7 @@ import {
         actions: [
           {
             label: "Join",
-            href: ``,
+            href: request.url
           },
         ],
       },
@@ -32,6 +32,40 @@ import {
 
     return response
   
+  }
+  
+  export async function POST(request: Request) {
+    const requestURL = new URL(request.url);
+    const id = requestURL.searchParams.get('id');
+    const daoIndex = parseInt(id ?? "");
+  
+    if (isNaN(daoIndex) || !DAOs[daoIndex]) {
+      return new Response("Invalid DAO ID", { status: 400 });
+    }
+  
+    DAOs[daoIndex].count += 1;
+  
+    const dao = DAOs[daoIndex];
+    const { count, fractions, asset } = dao;
+    const iconURL = new URL(asset ?? "", requestURL.origin);
+  
+    const payload: ActionGetResponse = {
+      icon: iconURL.toString(),
+      description: `Join this DAO. Current members: ${count}/${fractions}`,
+      title: "Join DAO",
+      label: "Join DAO",
+      links: {
+        actions: [
+          {
+            label: "Join",
+            href: request.url,
+          },
+        ],
+      },
+    };
+  
+    const response = Response.json(payload, { headers: ACTIONS_CORS_HEADERS });
+    return response;
   }
 
   export async function OPTIONS(request: Request) {
